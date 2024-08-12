f55b1c2a8ac7aff9e1e6333a64057b8ad019c795
khmgobe <dr94406@naver.com>
2024-08-12 22:28:46 +0900

    feat: var, val

---
+++ src/chap1/EmptyProgram.kt
@@ -0,0 +1,6 @@
+package chap1
+
+fun main() {
+    // 코틀린에는 세미콜론이 필요 없다.
+    println("Hello, World!")
+}
---
+++ src/chap1/VarAndVal/Vars.kt
@@ -0,0 +1,16 @@
+package chap1.VarAndVal
+
+fun main() {
+
+    /* 변할 수 있는 값 이라는 이름처럼, 저장된 값은 달라질 수 있다.
+       따라서 var 변수를 가변(mutable) 이라고 한다.
+     */
+
+    var whole = 11;
+    var fractional = 1.4
+    var words = "Twas Briling"
+    println(whole)
+    println(fractional)
+    println(words)
+
+}
